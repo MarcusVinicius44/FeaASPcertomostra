6226ecfb53773551b6f09e0fd929bc4740167183
MarcusVinicius44 <101570347+MarcusVinicius44@users.noreply.github.com>
2024-10-03 22:47:48 -0300

    commit message

---
+++ FeaASP/Pages/eventos.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FEAasp.Pages
+{
+    public class EventsModel : PageModel
+    {
+        private readonly ILogger<EventsModel> _logger;
+
+        public EventsModel(ILogger<EventsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
